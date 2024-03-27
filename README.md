d966cc7a0abb4293f6d390314427d4d8b3128cdf
Tejveer Singh <Tejveer.Singh@BRILLIO.COM>
2024-03-27 19:27:27 +0530

    Added section 2

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # python3-beginner
 It will contain all the code snippet written while python 3 course 
-# Checking dummy commit
---
+++ Section2/variables.py
@@ -0,0 +1,30 @@
+# Examples
+language = 'Python'
+lang = "Python"
+version = "3.11.3"
+print(language)
+print(lang)
+
+# Dynamic Typing
+number = 10
+print(type(number))
+number = 'ten'
+print(type(number))
+
+# variable can't start with number 
+# ex - 4_four 
+# variable name can't be reserved keyword
+# ex - for = 4 
+four = 4
+print(four)
+
+# Multiple assignments 
+one, two, three = 1,2,3
+print(one)
+print(two)
+print(three)
+
+# good and bad variable naming
+
+lname = 'Singh' # bad naming
+last_name = 'Singh' # good naming
